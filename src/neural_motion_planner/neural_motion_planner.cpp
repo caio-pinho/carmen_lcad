64243ba973f06a2c2ca86a581806aff2c5bc581b
caio-pinho <caio_pinho@hotmail.com>
2024-05-06 23:09:06 -0300

    funcionando, retirando funcoes nao usadas do planner

--- src/neural_motion_planner/neural_motion_planner.cpp
+++ src/neural_motion_planner/neural_motion_planner.cpp
@@ -36,119 +36,6 @@ extern int use_unity_simulator;
 
 
 void
-plot_path_and_colisions_points(vector<carmen_ackerman_path_point_t> &robot_path, vector<carmen_ackerman_path_point_t> &collision_points)
-{
-//	plot data Table - Last TCP - Optmizer tcp - Lane
-	//Plot Optmizer step tcp and lane?
-
-	#define DELTA_T (1.0 / 40.0)
-
-//	#define PAST_SIZE 300
-	static bool first_time = true;
-	static FILE *gnuplot_pipeMP;
-
-
-	if (first_time)
-	{
-		first_time = false;
-
-		gnuplot_pipeMP = popen("gnuplot", "w"); // -persist to keep last plot after program closes
-		fprintf(gnuplot_pipeMP, "set xrange [0:70]\n");
-		fprintf(gnuplot_pipeMP, "set yrange [-10:10]\n");
-//		fprintf(gnuplot_pipe, "set y2range [-0.55:0.55]\n");
-		fprintf(gnuplot_pipeMP, "set xlabel 'senconds'\n");
-		fprintf(gnuplot_pipeMP, "set ylabel 'effort'\n");
-//		fprintf(gnuplot_pipe, "set y2label 'phi (radians)'\n");
-//		fprintf(gnuplot_pipe, "set ytics nomirror\n");
-//		fprintf(gnuplot_pipe, "set y2tics\n");
-		fprintf(gnuplot_pipeMP, "set tics out\n");
-	}
-
-	FILE *gnuplot_data_file = fopen("gnuplot_data_path.txt", "w");
-	FILE *gnuplot_data_lane = fopen("gnuplot_data_colision.txt", "w");
-
-	for (unsigned int i = 0; i < robot_path.size(); i++)
-		fprintf(gnuplot_data_file, "%lf %lf %lf %lf %lf %lf %lf\n", robot_path.at(i).x, robot_path.at(i).y, 1.0 * cos(robot_path.at(i).theta), 1.0 * sin(robot_path.at(i).theta), robot_path.at(i).theta, robot_path.at(i).phi, robot_path.at(i).time);
-	for (unsigned int i = 0; i < collision_points.size(); i++)
-		fprintf(gnuplot_data_lane, "%lf %lf\n", collision_points.at(i).x, collision_points.at(i).y);
-//		fprintf(gnuplot_data_lane, "%lf %lf %lf %lf %lf %lf %lf\n", collision_points.at(i).x, collision_points.at(i).y, 1.0 * cos(collision_points.at(i).theta), 1.0 * sin(collision_points.at(i).theta), collision_points.at(i).theta, collision_points.at(i).phi, collision_points.at(i).time);
-
-	fclose(gnuplot_data_file);
-	fclose(gnuplot_data_lane);
-
-//	fprintf(gnuplot_pipe, "unset arrow\nset arrow from %lf, %lf to %lf, %lf nohead\n",0, -60.0, 0, 60.0);
-
-	fprintf(gnuplot_pipeMP, "plot "
-			"'./gnuplot_data_path.txt' using 1:2 w lines title 'robot_path',"
-			"'./gnuplot_data_colision.txt' using 1:2 title 'Collision'\n");
-
-	fflush(gnuplot_pipeMP);
-}
-
-
-void
-plot_state_goals(vector<carmen_ackerman_path_point_t> &pOTCP, vector<carmen_ackerman_path_point_t> &pLane,
-		  vector<carmen_ackerman_path_point_t> &pSeed, vector<Pose> &pGoals)
-{
-//	plot data Table - Last TCP - Optmizer tcp - Lane
-	//Plot Optmizer step tcp and lane?
-
-	#define DELTA_T (1.0 / 40.0)
-
-//	#define PAST_SIZE 300
-	static bool first_time = true;
-	static FILE *gnuplot_pipeMP;
-
-
-	if (first_time)
-	{
-		first_time = false;
-
-		gnuplot_pipeMP = popen("gnuplot", "w"); // -persist to keep last plot after program closes
-		fprintf(gnuplot_pipeMP, "set xrange [0:70]\n");
-		fprintf(gnuplot_pipeMP, "set yrange [-10:10]\n");
-//		fprintf(gnuplot_pipe, "set y2range [-0.55:0.55]\n");
-		fprintf(gnuplot_pipeMP, "set xlabel 'senconds'\n");
-		fprintf(gnuplot_pipeMP, "set ylabel 'effort'\n");
-//		fprintf(gnuplot_pipe, "set y2label 'phi (radians)'\n");
-//		fprintf(gnuplot_pipe, "set ytics nomirror\n");
-//		fprintf(gnuplot_pipe, "set y2tics\n");
-		fprintf(gnuplot_pipeMP, "set tics out\n");
-	}
-
-	FILE *gnuplot_data_file = fopen("gnuplot_data.txt", "w");
-	FILE *gnuplot_data_lane = fopen("gnuplot_data_lane.txt", "w");
-	FILE *gnuplot_data_seed = fopen("gnuplot_data_seed.txt", "w");
-	FILE *gnuplot_data_goals = fopen("gnuplot_data_goals.txt", "w");
-
-
-	for (unsigned int i = 0; i < pOTCP.size(); i++)
-		fprintf(gnuplot_data_file, "%lf %lf %lf %lf %lf %lf %lf\n", pOTCP.at(i).x, pOTCP.at(i).y, 1.0 * cos(pOTCP.at(i).theta), 1.0 * sin(pOTCP.at(i).theta), pOTCP.at(i).theta, pOTCP.at(i).phi, pOTCP.at(i).time);
-	for (unsigned int i = 0; i < pLane.size(); i++)
-		fprintf(gnuplot_data_lane, "%lf %lf %lf %lf %lf %lf %lf\n", pLane.at(i).x, pLane.at(i).y, 1.0 * cos(pLane.at(i).theta), 1.0 * sin(pLane.at(i).theta), pLane.at(i).theta, pLane.at(i).phi, pLane.at(i).time);
-	for (unsigned int i = 0; i < pSeed.size(); i++)
-		fprintf(gnuplot_data_seed, "%lf %lf\n", pSeed.at(i).x, pSeed.at(i).y);
-	for (unsigned int i = 0; i < pGoals.size(); i++)
-		fprintf(gnuplot_data_goals, "%lf %lf %lf %lf %lf\n", pGoals.at(i).x, pGoals.at(i).y, 1.0 * cos(pGoals.at(i).theta), 1.0 * sin(pGoals.at(i).theta), pGoals.at(i).theta);
-
-	fclose(gnuplot_data_file);
-	fclose(gnuplot_data_lane);
-	fclose(gnuplot_data_seed);
-	fclose(gnuplot_data_goals);
-
-//	fprintf(gnuplot_pipe, "unset arrow\nset arrow from %lf, %lf to %lf, %lf nohead\n",0, -60.0, 0, 60.0);
-
-	fprintf(gnuplot_pipeMP, "plot "
-			"'./gnuplot_data.txt' using 1:2:3:4 w vec size 0.3, 10 filled title 'OTCP',"
-			"'./gnuplot_data_lane.txt' using 1:2:3:4 w vec size 0.3, 10 filled title 'Lane',"
-			"'./gnuplot_data_goals.txt' using 1:2:3:4 w vec size 0.3, 10 filled title 'Goals',"
-			"'./gnuplot_data_seed.txt' using 1:2 with lines title 'Seed' axes x1y1\n");
-
-	fflush(gnuplot_pipeMP);
-}
-
-
-void
 move_poses_foward_to_local_reference(SE2 &robot_pose, double beta, carmen_behavior_selector_path_goals_and_annotations_message *path_goals_and_annotations_message,
 		vector<carmen_robot_and_trailer_path_point_t> *lane_in_local_pose)
 {
@@ -277,19 +164,6 @@ build_detailed_path_lane(vector<carmen_robot_and_trailer_path_point_t> *lane_in_
 }
 
 
-void
-compute_path_phis(vector<carmen_robot_and_trailer_path_point_t> *lane_in_local_pose)
-{
-	double L = GlobalState::robot_config.distance_between_front_and_rear_axles;
-	for (unsigned int i = 0; i < lane_in_local_pose->size() - 1; i++)
-	{
-		double delta_theta = carmen_normalize_theta(lane_in_local_pose->at(i + 1).theta - lane_in_local_pose->at(i).theta);
-		double l = DIST2D(lane_in_local_pose->at(i), lane_in_local_pose->at(i + 1));
-		lane_in_local_pose->at(i).phi = atan(L * (delta_theta / l));
-	}
-}
-
-
 bool
 build_detailed_rddf_lane(Pose *goal_pose, vector<carmen_robot_and_trailer_path_point_t> *lane_in_local_pose,
 		vector<carmen_robot_and_trailer_path_point_t> &detailed_lane)
@@ -304,7 +178,6 @@ build_detailed_rddf_lane(Pose *goal_pose, vector<carmen_robot_and_trailer_path_p
 
 		goal_in_lane = make_detailed_lane_start_at_car_pose(detailed_lane, temp_detail, goal_pose);
 
-//		compute_path_phis(lane_in_local_pose);
 	}
 	else
 	{
@@ -314,145 +187,6 @@ build_detailed_rddf_lane(Pose *goal_pose, vector<carmen_robot_and_trailer_path_p
 	}
 
 	return (goal_in_lane);
-}
-
-
-void
-filter_path_old(vector<carmen_ackerman_path_point_t> &path)
-{
-	unsigned int i;
-
-	if (path.size() < 1)
-		return;
-
-	for (i = 0; i < path.size(); i += 2)
-		if ((i + 1) < path.size())
-			path[i].time += path[i + 1].time;
-	for (i = 1; i < path.size(); i += 2)
-		path.erase(path.begin() + i);
-
-	for (i = 0; i < path.size(); i += 2)
-		if ((i + 1) < path.size())
-			path[i].time += path[i + 1].time;
-	for (i = 1; i < path.size(); i += 2)
-		path.erase(path.begin() + i);
-
-	if (GlobalState::ford_escape_online)
-	{
-		for (i = 0; i < path.size(); i += 2)
-			if ((i + 1) < path.size())
-				path[i].time += path[i + 1].time;
-		for (i = 1; i < path.size(); i += 2)
-			path.erase(path.begin() + i);
-	}
-}
-
-
-void
-remove_some_poses_at_the_end_of_the_path(vector<carmen_ackerman_path_point_t> &path)
-{
-	double path_time = 0.0;
-
-	while ((path.size() > 1) && (path_time < 1.0 / 10.0))
-	{
-		path_time += path[path.size() - 1].time;
-		path.pop_back();
-	}
-}
-
-
-void
-filter_path(vector<carmen_robot_and_trailer_path_point_t> &path)
-{
-	if (path.size() < 1)
-		return;
-
-	unsigned int i, count;
-	double path_time;
-
-	path_time = 0.0;
-	count = 0;
-	for (i = 0; (i < path.size()) && (path_time < 0.6); i += 2)
-	{
-		if ((i + 1) < path.size())
-		{
-			path[i].time += path[i + 1].time;
-			path_time += path[i].time;
-			count++;
-		}
-	}
-	for (i = 1; (i < path.size()) && (i < (count + 1)); i += 2)
-		path.erase(path.begin() + i);
-
-	path_time = 0.0;
-	count = 0;
-	for (i = 0; (i < path.size()) && (path_time < 0.6); i += 2)
-	{
-		if ((i + 1) < path.size())
-		{
-			path[i].time += path[i + 1].time;
-			path_time += path[i].time;
-			count++;
-		}
-	}
-	for (i = 1; (i < path.size()) && (i < (count + 1)); i += 2)
-		path.erase(path.begin() + i);
-
-	if (GlobalState::ford_escape_online)
-	{
-		path_time = 0.0;
-		count = 0;
-		for (i = 0; (i < path.size()) && (path_time < 0.6); i += 2)
-		{
-			if ((i + 1) < path.size())
-			{
-				path[i].time += path[i + 1].time;
-				path_time += path[i].time;
-				count++;
-			}
-		}
-		for (i = 1; (i < path.size()) && (i < (count + 1)); i += 2)
-			path.erase(path.begin() + i);
-	}
-}
-
-
-void
-apply_system_latencies(vector<carmen_ackerman_path_point_t> &path)
-{
-	unsigned int i, j;
-
-	for (i = 0; i < path.size(); i++)
-	{
-		j = i;
-		for (double lat = 0.0; lat < 0.3; j++)
-		{
-			if (j >= path.size())
-				break;
-			lat += path[j].time;
-		}
-		if (j >= path.size())
-			break;
-		path[i].phi = path[j].phi;
-	}
-	//    while (i < path.size())
-		//    	path.pop_back();
-
-	for (i = 0; i < path.size(); i++)
-	{
-		j = i;
-		for (double lat = 0.0; lat < 0.6; j++)
-		{
-			if (j >= path.size())
-				break;
-			lat += path[j].time;
-		}
-		if (j >= path.size())
-			break;
-		path[i].v = path[j].v;
-	}
-	while (i < path.size())
-		path.pop_back();
 }
 
 
@@ -512,80 +246,6 @@ path_has_collision_or_phi_exceeded(vector<carmen_robot_and_trailer_path_point_t>
 }
 
 
-void
-put_shorter_path_in_front(vector<vector<carmen_ackerman_path_point_t> > &paths, int shorter_path)
-{
-	if ((paths.size() > 1) && (paths[0].size() == 0))
-	{
-		vector<carmen_ackerman_path_point_t> shoter_path;
-		shoter_path = paths[shorter_path];
-		paths.erase(paths.begin() + shorter_path);
-		paths.insert(paths.begin(), shoter_path);
-	}
-
-	unsigned int size, i, j;
-	size = paths.size();
-	for (i = j = 0; i < size; i++)
-	{
-		if (paths[j].size() == 0)
-		{
-			paths.erase(paths.begin() + j);
-		}
-		else
-			j++;
-	}
-}
-
-
-TrajectoryControlParameters
-get_shorter_path(int &shorter_path, int num_paths, vector<vector<carmen_ackerman_path_point_t> > paths,
-		const vector<TrajectoryControlParameters> otcps)
-{
-	double shorter_path_size = 1000.0;
-	TrajectoryControlParameters best_otcp;
-
-	for (int i = 0; i < num_paths; i++)
-	{
-		if ((paths[i].size() > 0) && (otcps[i].sf < shorter_path_size) && otcps[i].valid)
-		{
-			shorter_path_size = otcps[i].sf;
-			shorter_path = i;
-			best_otcp = otcps[i];
-		}
-	}
-	return best_otcp;
-}
-
-
-double
-get_intermediate_speed(double current_robot_pose_v, double v_goal, double dist_to_goal, double dist_to_curve)
-{
-	// https://www.wolframalpha.com/input/?i=solve+s%3Dg*(g-v)%2Fa%2B(v-g)*((g-v)%2F(2*a)))+for+a
-	// https://www.wolframalpha.com/input/?i=solve+s%3Dv*((g-v)%2Fa)%2B0.5*a*((g-v)%2Fa)%5E2+for+g
-
-	if (dist_to_goal > dist_to_curve)
-		return v_goal;
-
-	double v0 = current_robot_pose_v;
-	double a = (v_goal * v_goal - v0 * v0) / (2.0 * dist_to_curve);
-	double sqrt_val = 2.0 * a * dist_to_goal + v0 * v0;
-	double vg = v_goal;
-	if (sqrt_val > 0.0)
-		vg = sqrt(sqrt_val);
-	if (vg < v_goal)
-		vg = v_goal;
-
-//	static double first_time = 0.0;
-//	double t = carmen_get_time();
-//	if (first_time == 0.0)
-//		first_time = t;
-//	printf("t %.3lf, v0 %.1lf, va %.1lf, a %.3lf, vg %.2lf, dg %.1lf, da %.1lf\n", t - first_time, v0, va, a, vg, dg, da);
-//	printf("t %.3lf, v0 %.1lf, a %.3lf, vg %.2lf, dg %.1lf, tt %.3lf\n", t - first_time, v0, a, vg, dg, (vg - v0) / a);
-
-	return (vg);
-}
-
-
 bool
 get_path_from_optimized_tcp(vector<carmen_robot_and_trailer_path_point_t> &path,
 		vector<carmen_robot_and_trailer_path_point_t> &path_local,
@@ -601,87 +261,22 @@ get_path_from_optimized_tcp(vector<carmen_robot_and_trailer_path_point_t> &path,
 		path = simulate_car_from_parameters(td, otcp, td.v_i, td.beta_i, 0.02);
 	else
 		path = simulate_car_from_parameters(td, otcp, td.v_i, td.beta_i);
+
 	path_local = path;
+
 	if (path_has_loop(td.dist, otcp.sf))
 	{
 		printf(KRED "+++++++++++++ Path has loop...\n" RESET);
 		return (false);
 	}
 
-//	if (path_has_collision_or_phi_exceeded(path))
-//		return (false);
 	if (path_has_collision_or_phi_exceeded(path))
 		GlobalState::path_has_collision_or_phi_exceeded = true;
 	else
 		GlobalState::path_has_collision_or_phi_exceeded = false;
 
 	move_path_to_current_robot_pose(path, localizer_pose);
-
-//	printf("\n* MPP path\n");
-//	for (unsigned int i = 0; (i < path.size()) && (i < 15); i++)
-//	{
-//		printf("v %2.2lf, phi %5.3lf, t %5.3lf, x %5.3lf, y %5.3lf, theta %5.3lf\n",
-//				path[i].v, path[i].phi, path[i].time,
-//				path[i].x - localizer_pose->x, path[i].y - localizer_pose->y, path[i].theta);
-//	}
-//	fflush(stdout);
-
-	// Para evitar que o fim do path bata em obstáculos devido a atrazo na propagacao da posicao atual deles
-//	remove_some_poses_at_the_end_of_the_path(path);
-
-//	if (GlobalState::use_mpc)
-//		apply_system_latencies(path);
-//	else
-//		filter_path(path);
-	if (!GlobalState::use_mpc && !use_unity_simulator && !GlobalState::eliminate_path_follower)
-		filter_path(path);
-
 	return (true);
-}
-
-
-bool
-goal_pose_vector_too_different(Pose goal_pose, Pose localizer_pose)
-{
-	static Pose g_last_goal_pose;
-	bool too_different = false;
-
-	if (g_last_goal_pose.x == 0.0 && g_last_goal_pose.y == 0.0)
-	{
-		g_last_goal_pose = goal_pose;
-		return (true);
-	}
-
-	double distance_between_goals = g_last_goal_pose.distance(goal_pose);
-	double distance_to_goal = localizer_pose.distance(goal_pose);
-	if ((distance_between_goals / distance_to_goal) > 0.2) // 20%
-		too_different = true;
-
-	double angle_diff = g_last_goal_pose.get_theta_diff(goal_pose);
-	angle_diff = rad2deg(angle_diff);
-	if (angle_diff > 10.0)
-		too_different = true;
-
-	g_last_goal_pose = goal_pose;
-//	if (too_different)
-//		printf("TOO DIFFERENT!!!\n");
-
-	return (too_different);
-}
-
-bool
-goal_is_behind_car(Pose *localizer_pose, Pose *goal_pose)
-{//funcao tem que ser melhorada. Usar coordenadas polares pode ser melhor.
-	SE2 robot_pose(localizer_pose->x, localizer_pose->y, localizer_pose->theta);
-	SE2 goal_in_world_reference(goal_pose->x, goal_pose->y, goal_pose->theta);
-	SE2 goal_in_car_reference = robot_pose.inverse() * goal_in_world_reference;
-	double goal_x = goal_in_car_reference[0];
-	double goal_theta = goal_in_car_reference[2];
-
-	if(goal_x <= 0.0 && fabs(goal_theta) < M_PI_2)
-		return true;
-
-	return false;
 }
 
 
